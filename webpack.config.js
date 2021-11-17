015e2ffeada6f363c34fa112656b39b37e4bd516
jianfeng <gjf610@yeah.net>
2021-11-17 17:02:36 +0800

    added css extract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
     mode: 'development',
@@ -15,13 +15,29 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'xiao Wang',
             template: 'src/assets/index.html'
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+            chunkFilename: "[id].[contenthash].css",
+            ignoreOrder: false,
+        }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                // use: ['style-loader', 'css-loader']
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: "../",
+                            hmr: process.env.NODE_ENV === 'development'
+
+                        },
+                    },
+                    "css-loader",
+                ],
             }
         ]
     }
