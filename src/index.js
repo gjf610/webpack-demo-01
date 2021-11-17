7f6381e4b41f3dc81536914159290c8d601bf58e
jianfeng <gjf610@yeah.net>
2021-11-17 22:21:56 +0800

    added lazy module

--- src/index.js
+++ src/index.js
@@ -5,3 +5,18 @@ const div = document.getElementById('app')
 div.innerHTML = `
 <img src="${svg}">
 `
+
+const button = document.createElement('button')
+
+button.innerText = '懒加载'
+button.onclick = () => {
+    const promise = import('./lazy');
+    promise.then((module) => {
+        const fn = module.default
+        fn()
+    }, () => {
+
+    });
+}
+
+div.appendChild(button)
---
+++ src/lazy.js
@@ -0,0 +1,6 @@
+const lazy = () => {
+    console.log('我是一个懒加载的模块')
+}
+
+
+export default lazy;
